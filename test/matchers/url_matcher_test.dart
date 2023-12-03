1c6b3eb124fc39544bfef2401bef6b414ff66edb
Kabo <20254485+kaboc@users.noreply.github.com>
2023-12-03 18:48:29 +0900

    Reorganize url_matcher_test.dart

--- test/matchers/url_matcher_test.dart
+++ test/matchers/url_matcher_test.dart
@@ -15,6 +15,14 @@ void main() {
     expect(found, url);
   });
 
+  test('result does not contain strings before URL', () {
+    const url = 'https://example.com/';
+    const input = 'abc$url';
+    final matches = regExp.allMatches(input).toList();
+    final found = input.substring(matches[0].start, matches[0].end);
+    expect(found, url);
+  });
+
   test('supports both http and https', () {
     const urls = [
       'http://example.com/',
@@ -30,12 +38,41 @@ void main() {
     }
   });
 
-  test('result does not contain strings before URL', () {
-    const url = 'https://example.com/';
-    const input = 'abc$url';
+  test('does not match if TLD is only a single letter', () {
+    const url1 = 'https://example.c';
+    const url2 = 'https://example.c:8080';
+    const input = '$url1 $url2';
+    final matches = regExp.allMatches(input);
+    expect(matches, isEmpty);
+  });
+
+  test('does not match if TLD contains non-alphabets', () {
+    const urls = [
+      'https://example.c+m',
+      'https://example.c+m/',
+      'https://example.c=m',
+      'https://example.c=m',
+    ];
+    final input = urls.join(' ');
+    final matches = regExp.allMatches(input);
+    expect(matches, isEmpty);
+  });
+
+  test('excludes non-alphabets (other than /#?) at the end of TLD', () {
+    const url = 'https://example.com';
+    const urls = ['$url+', '$url=', '$url%'];
+    final input = urls.join(' ');
     final matches = regExp.allMatches(input).toList();
     final found = input.substring(matches[0].start, matches[0].end);
     expect(found, url);
+  });
+
+  test('does not match if host ends with a dot', () {
+    const url1 = 'https://example.';
+    const url2 = 'https://example.:8080';
+    const input = '$url1 $url2';
+    final matches = regExp.allMatches(input);
+    expect(matches, isEmpty);
   });
 
   test('dot at the end is excluded', () {
@@ -51,7 +88,7 @@ void main() {
     expect(found2, equals(url));
   });
 
-  test('path containing a dot is not excluded', () {
+  test('path can contain dots', () {
     const url = 'https://example.com/foo.jpg';
     const input = '111 $url 222';
     final matches = regExp.allMatches(input).toList();
@@ -59,7 +96,7 @@ void main() {
     expect(found, url);
   });
 
-  test('backslashes are excluded', () {
+  test('backslashes are not considered part of URL', () {
     const url = r'https://example.com/foo\\bar.jpg\\';
     final matches = regExp.allMatches(url).toList();
     expect(matches, hasLength(1));
@@ -133,45 +170,8 @@ void main() {
     }
   });
 
-  test('does not match if TLD is only a single letter', () {
-    const url1 = 'https://example.c';
-    const url2 = 'https://example.c:8080';
-    const input = '$url1 $url2';
-    final matches = regExp.allMatches(input);
-    expect(matches, isEmpty);
-  });
-
-  test('does not match if TLD contains non-alphabets', () {
-    const urls = [
-      'https://example.c+m',
-      'https://example.c+m/',
-      'https://example.c=m',
-      'https://example.c=m',
-    ];
-    final input = urls.join(' ');
-    final matches = regExp.allMatches(input);
-    expect(matches, isEmpty);
-  });
-
-  test('excludes non-alphabets (other than /#?) at the end of TLD', () {
-    const url = 'https://example.com';
-    const urls = ['$url+', '$url=', '$url%'];
-    final input = urls.join(' ');
-    final matches = regExp.allMatches(input).toList();
-    final found = input.substring(matches[0].start, matches[0].end);
-    expect(found, url);
-  });
-
-  test('does not match if host ends with a dot', () {
-    const url1 = 'https://example.';
-    const url2 = 'https://example.:8080';
-    const input = '$url1 $url2';
-    final matches = regExp.allMatches(input);
-    expect(matches, isEmpty);
-  });
-
-  test('no group', () {
-    const url = 'https://example.com/';
+  test('result has no group', () {
+    const url = 'https://example.com/aaa/bbb';
     final matches = regExp.allMatches(url).toList();
     expect(matches, hasLength(1));
     expect(matches[0].groupCount, 0);
